164760aaf7aacb7ef56173bcfdaa357ea8e11923
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-24 18:01:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#take user input
+#Convert input to str
+userInput = str (input(" length = ..."))
+userInput = str (input(" char = ..."))
+#variable 0
+d_0 = ""+userInput+""
+
+#variable 1
+d_1 = ""+userInput+""+userInput+""
+
+#variable 2
+d_2 = ""+userInput+""+userInput+""+userInput+""
+
+#variable 3
+d_3 = ""+userInput+""+userInput+""+userInput+""+userInput+""
+
+#variable 4
+d_4 = ""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""
+
+#variable 5
+d_5 = ""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""
+#print , 1.2.3.4.1.2.3.4
+print(d_0)
+print(d_1)
+print(d_2)
+print(d_3)
+print(d_4)
+print(d_5)
+
+
+d_0 = ""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""
+
+d_1 = ""+userInput+""+userInput+""+userInput+""+userInput+""+userInput+""
+
+d_2 = ""+userInput+""+userInput+""+userInput+""+userInput+""
+
+d_3 = ""+userInput+""+userInput+""+userInput+""
+
+d_4 =  ""+userInput+""+userInput+""
+
+d_5 = ""+userInput+"" 
+
+print(d_0)
+print(d_1)
+print(d_2)
+print(d_3)
+print(d_4)
+print(d_5)
+
